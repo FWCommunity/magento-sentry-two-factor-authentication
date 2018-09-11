8d7ebf9f8575de1005151b82041a3dae49f1c94a
R-to-the-K <robert.krule@fwmedia.com>
2018-09-10 21:40:07 -0400

    Update adminpanel.js

--- js/he_twofactor/adminpanel.js
+++ js/he_twofactor/adminpanel.js
@@ -1,6 +1,5 @@
 //jquery substitute for document ready (no guarantee that either jquery OR prototype will be available)
-(function(g,b){function c(){if(!e){e=!0;for(var a=0;a<d.length;a++)d[a].fn.call(window,d[a].ctx);d=[]}}function h(){"complete"===document.readyState&&c()}b=b||window;var d=[],e=!1,f=!1;b[g||"he_twofactor_ready"]=function(a,b){e?setTimeout(function(){a(b)},1):(d.push({fn:a,ctx:b}),"complete"===document.readyState?setTimeout(c,1):f||(document.addEventListener?(document.addEventListener("DOMContentLoaded",c,!1),window.addEventListener("load",c,!1)):(document.attachEvent("onreadystatechange",h),window.attachEvent("onload",
-c)),f=!0))}})("he_twofactor_ready",window);
+(function(g,b){function c(){if(!e){e=!0;for(var a=0;a<d.length;a++)d[a].fn.call(window,d[a].ctx);d=[]}}function h(){"complete"===document.readyState&&c()}b=b||window;var d=[],e=!1,f=!1;b[g||"he_twofactor_ready"]=function(a,b){e?setTimeout(function(){a(b)},1):(d.push({fn:a,ctx:b}),"complete"===document.readyState?setTimeout(c,1):f||(document.addEventListener?(document.addEventListener("DOMContentLoaded",c,!1),window.addEventListener("load",c,!1)):(document.attachEvent("onreadystatechange",h),window.attachEvent("onload",c)),f=!0))}})("he_twofactor_ready",window);
 //on document ready
 he_twofactor_ready(function() {
   //if the provider selector is on this page
